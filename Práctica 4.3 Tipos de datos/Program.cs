44f9dd5ee5b02459b4cf25fa7d019bf848dfd732
Cristina Cañadas Gómez - Limón <rosten1805@protonmail.com>
2023-02-05 18:35:14 +0100

    Agregar archivos de proyecto.

---
+++ Práctica 4.3 Tipos de datos/Program.cs
@@ -0,0 +1,24 @@
+﻿int num1, num2;
+
+Console.WriteLine("Introduce el primer número entero:");
+num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduce el segundo número entero:");
+num2 = int.Parse(Console.ReadLine());
+
+int suma = num1 + num2;
+int resta = num1 - num2;
+int producto = num1 * num2;
+int cociente = num1 / num2;
+int residuo = num1 % num2;
+
+Console.WriteLine("Primer número: " + num1);
+Console.WriteLine("Segundo número: " + num2);
+Console.WriteLine("La suma es: " + suma);
+Console.WriteLine("La resta es: " + num1 + " - " + num2 + " = " + resta);
+Console.WriteLine("La multiplicación es: " + producto);
+Console.WriteLine("La división es: " + cociente);
+Console.WriteLine("El residuo es: " + residuo);
+
+Console.WriteLine("Pulse una tecla para continuar.");
+Console.ReadLine();
